a878e60ce263d66b2bf71d6408bf44fe39458a75
Huy Nguyen <quanghuynguyen040197@gmail.com>
2020-07-31 20:20:54 +0700

    add say hi method for Dog

--- dog.js
+++ dog.js
@@ -1,4 +1,6 @@
-function Dog(){
+var chalk = require('chalk');
+function Dog(name){
+	this.name = name;
 	this.stomatch = [];
 }
 
@@ -6,3 +8,10 @@ Dog.prototype.eat = function(bone) {
 	// body...
 	this.stomatch.push(bone);
 };
+
+Dog.prototype.sayHi = function() {
+	// body...
+	console.log('hi my name is: ' + chalk.blue(this.name));
+};
+
+module.exports = Dog;
