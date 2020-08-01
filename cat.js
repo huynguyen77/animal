c121d0c44d30cc3e3b0a28991740c0b2980e6746
Huy Nguyen <quanghuynguyen040197@gmail.com>
2020-08-01 12:37:05 +0700

    Cat eat Mouse

--- cat.js
+++ cat.js
@@ -5,6 +5,7 @@ function Cat (){
 Cat.prototype.eat = function(mouse) {
 	// body...
 	this.stomatch.push(mouse);
+	mouse.die();
 };
 
 module.exports = Cat;
--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 var Dog = require('./dog');
+var Mouse = require('./mouse');
+var Cat = require('./cat');
 
 var dog = new Dog('muc');
+var tom = new Cat();
+var mouse = new Mouse('jerry');
+
 
 dog.sayHi();
+tom.eat(mouse);
+console.log(tom);
